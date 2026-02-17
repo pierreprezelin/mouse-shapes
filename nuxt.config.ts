511b94500eeede9736c3fe8e9757d3dfd493b196
Pierre Prezelin <pierreprezelin.pro@gmail.com>
2026-02-17 13:27:09 -0500

    build: remove unused nuxt modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,6 @@ export default defineNuxtConfig({
   modules: [
     "@nuxt/eslint",
     "@nuxt/ui",
-    "@nuxt/a11y",
-    "@nuxt/hints",
-    "@nuxt/image",
-    "@nuxt/fonts",
     "@nuxtjs/supabase",
   ],
   devtools: {
