05e8484852f8fc96fbda1121d9ef164279a673ae
Pierre Prezelin <pierreprezelin.pro@gmail.com>
2026-02-18 15:21:16 -0500

    test: remove index route prerendering

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,9 +9,6 @@ export default defineNuxtConfig({
     enabled: true,
   },
   css: ["~/assets/css/main.css"],
-  routeRules: {
-    "/": { prerender: true },
-  },
   compatibilityDate: "2025-01-15",
   eslint: {
     config: {
